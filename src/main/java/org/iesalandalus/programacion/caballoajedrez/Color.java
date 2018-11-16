74dc6df459c26de6a36f6285652fb9be4e4db8e7
Usuario <Usuario@Portatil3>
2018-11-16 18:28:13 +0100

    Creo el enumerado Color

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Color.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Usuario
+ */
+
+//Creamos Enumerado Color   
+   
+    public enum Color 
+{
+    Blanco,Negro;
+}
+
