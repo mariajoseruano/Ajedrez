95ffbb57a1377d8d93a991e08eae08cb03d5a875
Usuario <Usuario@Portatil3>
2018-11-16 18:46:53 +0100

    Creo la clase Posición con sus atributos fila y columna

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Posicion {
+    
+    private int fila;
+    private char columna;
+    
+    
+}
