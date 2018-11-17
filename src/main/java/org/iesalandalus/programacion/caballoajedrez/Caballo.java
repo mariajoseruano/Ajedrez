426df57775dc535ad99c037edf1d656f535ec884
Usuario <Usuario@Portatil3>
2018-11-17 18:29:56 +0100

    Ejercicio 12. Crear el método get y set para cada atributo.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -13,6 +13,37 @@ public class Caballo {
     
     private Color color; // enumerado
     private Posicion posicion; //Clase posicion
-            
+     
+       
+// Ejercicio 12: Método get y set para cada atributo
+    
+//Getter fila:Método que devuelve el valor de un color
+    public Color getColor(){
+	return color;
+    }
+
+    //Getter posicion:Método que devuelve el valor de una posicion
+    public Posicion getPosicion(){
+	return posicion;
+    }
+
+    // Setter color:Método que modifica el color
+     public void setColor(Color color) 
+    {
+        if (color==null)
+            throw new IllegalArgumentException("ERROR: No es posible asignar un color nulo.");
+        
+        this.color=color;           
+    }
+   // Setter columna;Método que modifica una posicion
+     
+     public void setPosicion(Posicion posicion) 
+    {
+      //Comprobamos que el objeto pasado como parámetro no es nulo
+        if (posicion==null)
+            throw new IllegalArgumentException("ERROR: La posicion pasada como parámetro es nula");
+        
+        this.posicion= posicion;
+    }
     
 }
