c6adb676769c0d57ae355c8100e4da5a944289d8
Usuario <Usuario@Portatil3>
2018-11-17 12:14:37 +0100

    Ejercicio 6.Creo los métodos set y get

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -13,6 +13,36 @@ public class Posicion {
     
     private int fila;
     private char columna;
+
+    // Constructor que inicializa todos los atributos a través de parámetros
     
-    
+       
+    //Getter fila:Método que devuelve el valor de una fila
+    public int getfila(){
+	return fila;
+    }
+
+    //Getter columna:Método que devuelve el valor de una columna
+    public char getcolumna(){
+	return columna;
+    }
+
+    // Setter fila:Método que modifica la fila
+     public void setfila(int fila) 
+    {
+        if (fila<=0 || fila>8)
+            this.fila = fila;
+        else
+     
+            throw new IllegalArgumentException("La fila introducida como parámetro no es válida");            
+    }
+   // Setter columna
+     public void setcolumna(char columna) 
+    {
+        if (columna<a || columna>=i)
+            this.columna= columna;
+        else
+            throw new IllegalArgumentException("La columna introducida como parámetro no es válida");            
+    }
+        
 }
