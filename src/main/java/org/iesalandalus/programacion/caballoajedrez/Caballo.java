f0dc21bb50fd300b15d0c53026f66e7181091331
Usuario <Usuario@Portatil3>
2018-11-25 09:36:06 +0100

    Clase Caballo: Ejercicio 11 al 19.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -5,6 +5,8 @@
  */
 package org.iesalandalus.programacion.caballoajedrez;
 
+import java.util.Objects;
+
 /**
  *
  * @author Usuario
@@ -13,6 +15,8 @@ public class Caballo {
     
     private Color color; // enumerado
     private Posicion posicion; //Clase posicion
+    
+    
      
        
 // Ejercicio 12: Método get y set para cada atributo
@@ -45,5 +49,153 @@ public class Caballo {
         
         this.posicion= posicion;
     }
+     
+     //Ejericio 13:Crea un contructor por defecto que cree un caballo negro en la posicion 8b.
+     
+     public Caballo()
+     {
+		color= Color.NEGRO;
+                posicion=new Posicion(8,'b');
+               
+     }
+     /*Ejercicio 14: Crea un Constructor de clase que acepte como parametro el color que creará
+     un caballo de dicho color cuya posicion es 1b si es blanco 0 8b si es negro.*/
+     
+     public Caballo(Color color )
+     {
+        if (color ==Color.BLANCO)
+        {
+            posicion=new Posicion(1,'b');
+            this.color=color;
+        }
+        else 
+        {
+            posicion=new Posicion(8,'b');
+            this.color=color;
+        }
+     }      
+     /*  Ejercicio 15 crea un constructor que acepte como parametro color y columna inicial. 
+     la columna inicial debe de ser la b o la g( de lo contrario debe de lanzar una excepcion 
+     y crear un caballo del color dado y colocado en dicha columna y cuya fila será la 1 
+     si es blanco y la 8 si es negro.
+     */            
+           
+    public Caballo ( Color color,char columnaInicial) 
+    {
+        if (columnaInicial=='b' || columnaInicial=='g')
+        {
+            this.color=color;
+            if (color==Color.BLANCO)                   
+                posicion=new Posicion(1,columnaInicial);
+            else
+                posicion= new Posicion(8,columnaInicial);                     
+          
+           
+        }
+        else
+            throw new IllegalArgumentException("La Columna Inicial introducida no es correcta");          
+    } 
+           
+                
+                   
+                
+                 
+    /*Ejercicio 17 Método mover: dependiendo del movimiento modificará la posicion del mismo
+     o si no puede hacer el movimiento debe de lanzar una excepcion.
+        Declaramos char c='A'; int i=(int)c;
+     */   
+     
+     public void mover( Direccion dir)
+     {
+        int filaActual,nuevaFila=1;
+        char columnaActual,nuevaColumna='a';
+        
+        filaActual=posicion.getFila();
+        columnaActual=posicion.getColumna();
+               
+              
+         
+         switch (dir)
+         {
+             case ARRIBA_IZQUIERDA:
+                 nuevaFila=filaActual+2;
+                 nuevaColumna=(char)(columnaActual-1);
+                 break;
+             case ARRIBA_DERECHA:
+                 nuevaFila=filaActual+2;
+                 nuevaColumna=(char)(columnaActual+1);
+                 break;
+            case DERECHA_ARRIBA:
+                 nuevaFila=filaActual+1;
+                 nuevaColumna=(char)(columnaActual+2);
+                 break;  
+            case DERECHA_ABAJO:
+                 nuevaFila=filaActual-1;
+                 nuevaColumna=(char)(columnaActual+2);
+                 break;  
+            case ABAJO_DERECHA:
+                 nuevaFila=filaActual-2;
+                 nuevaColumna=(char)(columnaActual+1);
+                 break;  
+             case ABAJO_IZQUIERDA:
+                 nuevaFila=filaActual-2;
+                 nuevaColumna=(char)(columnaActual-1);
+                 break;  
+             case IZQUIERDA_ARRIBA:
+                 nuevaFila=filaActual+1;
+                 nuevaColumna=(char)(columnaActual-2);
+                 break;  
+             case IZQUIERDA_ABAJO:
+                 nuevaFila=filaActual-1;
+                 nuevaColumna=(char)(columnaActual-2);
+                 break; 
+             default:
+                 //throw new OperationNotSupportedException("ERROR: No se puede realizar este movimiento");
+            
+         }         
+         
+         posicion= new Posicion(nuevaFila,nuevaColumna);
+    }
     
+    //Ejercicio 18 Crea método equals para comprar la igualdad de objetos de clase.
+             
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.color);
+        hash = 89 * hash + Objects.hashCode(this.posicion);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Caballo other = (Caballo) obj;
+        if (this.color != other.color) {
+            return false;
+        }
+        if (!Objects.equals(this.posicion, other.posicion)) {
+            return false;
+        }
+        return true;
+    }
+
+    //Ejercicio 19: Método toString devolver un String y será la representación del objeto.
+    @Override
+    public String toString() {
+        return "Caballo{" + "color=" + color + ", posicion=" + posicion + '}';
+    }
+        
+    
+       
+       
 }
+
